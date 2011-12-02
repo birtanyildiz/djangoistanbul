66120a36c4c210c2c194ed5f84187c5f6d69185c
Cihan Okyay <okyaycihan@gmail.com>
2011-12-02 14:53:52 +0200

    birtakim template degisiklikleri

--- views.py
+++ views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render_to_response
 
+from events.models import *
+
 def home(request):
-    return render_to_response('index.html')
+    return render_to_response('index.html', {
+        'events': Events.objects.all(),
+    })
+
